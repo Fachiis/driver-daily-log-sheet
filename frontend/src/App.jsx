525b656a9d3256099e53c0b147243d75221234d4
Fachii Zasha <zashafachii@gmail.com>
2025-10-05 00:15:48 +0100

    add Prettier configuration and update project files for consistent formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,15 @@
 import './App.css'
-import {Button} from "@/components/ui/button.js";
+import { Button } from '@/components/ui/button.js'
 
 function App() {
-
-    return (
-        <>
-            <div className="text-3xl font-bold underline text-green-700">
-                Hello Vite + React!
-            </div>
-                <Button>
-                    Click me
-                </Button>
-        </>
-    )
+   return (
+      <>
+         <div className="text-3xl font-bold underline text-green-700">
+            Hello Vite + React!
+         </div>
+         <Button>Click me</Button>
+      </>
+   )
 }
 
 export default App
